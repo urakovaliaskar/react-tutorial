06f3439e73b419c39c89624b8f83f386cc9352de
urakovaliaskar <urakov.aliaskar@gmail.com>
2019-10-16 18:56:58 +0600

    Updates

---
+++ precache-manifest.b7bac55e305308169540a34de5219ac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf771381b6bb69c13d83f87dfaf42b65",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "2f17d52748e1ca9d10f5",
+    "url": "/react-tutorial/static/css/main.629b192f.chunk.css"
+  },
+  {
+    "revision": "1bcaae9235f43d21199e",
+    "url": "/react-tutorial/static/js/2.2a8636c6.chunk.js"
+  },
+  {
+    "revision": "2f17d52748e1ca9d10f5",
+    "url": "/react-tutorial/static/js/main.8f8c5498.chunk.js"
+  },
+  {
+    "revision": "acd1605a6af8f99b39b9",
+    "url": "/react-tutorial/static/js/runtime-main.b18dd318.js"
+  }
+]);
